e721d95a4914778ecb6c218c955a7e9bfcb87eda
constWardtz <edwardtutorials22@gmail.com>
2020-12-24 19:34:18 -0400

    Add on scroll function

--- js/scroll.js
+++ js/scroll.js
@@ -1,10 +1,10 @@
 const navbarContainer = document.querySelector(`#navbar-container`);
 const navbarContent = document.querySelector(`#navbar-content`);
+
 window.addEventListener("scroll", () => {
   if (window.scrollY > 300) {
     navbarContainer.style = `
         background-color: var(--dark-blue2);
-        margin-top: 0rem;
         height: 150px
         transition: height .3s;
     `;
